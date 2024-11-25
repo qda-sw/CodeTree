d40fedce5df98df7842fb44055aaccbf2fb51ff1
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 23:42:48 +0900

    Title: 정수 사각형 최대 합, Time: 67ms, Memory: 18MB, Status: Passed - CodeTree

--- 241125/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 241125/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -7,9 +7,17 @@ dxs = [0, -1]
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-for y in range(n):
-    for x in range(n):
+# for y in range(n):
+#     for x in range(n):
+#         arr[y][x] += max(
+#             [arr[y + dy][x + dx] for dy, dx in zip(dys, dxs) if can_move(y+dy, x+dx)] + [0]
+#         )
+for i in range(1, n):
+    arr[0][i] += arr[0][i-1]
+    arr[i][0] += arr[i-1][0]
+for y in range(1, n):
+    for x in range(1, n):
         arr[y][x] += max(
-            [arr[y + dy][x + dx] for dy, dx in zip(dys, dxs) if can_move(y+dy, x+dx)] + [0]
+            [arr[y + dy][x + dx] for dy, dx in zip(dys, dxs) if can_move(y+dy, x+dx)]
         )
 print(arr[-1][-1])
