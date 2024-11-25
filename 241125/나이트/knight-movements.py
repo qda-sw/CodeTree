be500d7570868f78d5b99e89ac7e90ff5c143a67
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 22:57:26 +0900

    Title: 나이트, Time: 71ms, Memory: 18MB, Status: Passed - CodeTree

--- 241125/나이트/knight-movements.py
+++ 241125/나이트/knight-movements.py
@@ -3,24 +3,24 @@ from collections import deque
 
 def bfs():
     def can_move(y, x):
-        return 0 <= y < n and 0 <= x < n and not visited[y][x]
+        return 0 <= y < n and 0 <= x < n and not (y, x) in visited
 
     dys = [1, 2, 2, 1, -1, -2, -2, -1]
     dxs = [2, 1, -1, -2, -2, -1, 1, 2]
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    steps = [[0 for _ in range(n)] for _ in range(n)]
+    visited = set()
     queue = deque()
-    queue.append((r1-1, c1-1))
-    visited[r1-1][c1-1] = True
+    queue.append((r1-1, c1-1, 0))
+    visited.add((r1-1, c1-1))
     while queue:
-        y, x = queue.popleft()
+        y, x, depth = queue.popleft()
+        if y == r2-1 and x == c2-1:
+            return depth
         for dy, dx in zip(dys, dxs):
             ny, nx = y + dy, x + dx
             if can_move(ny, nx):
-                visited[ny][nx] = True
-                steps[ny][nx] = steps[y][x] + 1
-                queue.append((ny, nx))
-    return steps[r2-1][c2-1] if visited[r2-1][c2-1] else -1
+                visited.add((ny, nx))
+                queue.append((ny, nx, depth+1))
+    return -1
 n = int(input())
 r1, c1, r2, c2 = map(int, input().split())
 print(bfs())
