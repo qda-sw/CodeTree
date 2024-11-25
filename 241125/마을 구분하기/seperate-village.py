56270b19ace31640f7c0edc468b6eea03f8b0a57
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 19:54:10 +0900

    Title: 마을 구분하기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241125/마을 구분하기/seperate-village.py
@@ -0,0 +1,27 @@
+dys = [0, 1]
+dxs = [1, 0]
+
+def can_move(y, x):
+    return 0 <= y < n and 0 <= x < n and arr[y][x] == 1
+
+def find_village(y, x):
+    if not can_move(y, x):
+        return 0
+    arr[y][x] = 0
+    population = 1
+    for dy, dx in zip(dys, dxs):
+        ny, nx = y + dy, x + dx
+        population += find_village(ny, nx)
+    return population
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+result = []
+for r in range(n):
+    for c in range(n):
+        temp = find_village(r, c)
+        if temp:
+            result.append(temp)
+print(len(result))
+print(*sorted(result), sep='\n')
