5f13b5dc195e7740fc92ca5b9d9228ca2f8ee01c
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 20:24:42 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 79ms, Memory: 18MB, Status: Passed - CodeTree

--- 241125/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 241125/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -9,21 +9,18 @@ def can_move(y, x):
 def bfs():
     while queue:
         y, x = queue.popleft()
-        arr[y][x] = 0
         for dy, dx in zip(dys, dxs):
             ny, nx = y + dy, x + dx
-            if not can_move(ny, nx) or visited[ny][nx]:
+            if not can_move(ny, nx):
                 continue
-            visited[ny][nx] = True
+            arr[ny][nx] = 0
             queue.append((ny, nx))
-
     
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 queue = deque()
-visited = [[False for _ in range(m)] for _ in range(n)]
+arr[0][0] = 0
 queue.append((0, 0))
-visited[0][0] = True
 bfs()
 print(1 if arr[n-1][m-1] == 0 else 0)
