680004f434b1dedecb4c626444b40b15f86d9a8b
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 23:23:10 +0900

    Title: 사각형 채우기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241125/사각형 채우기/rectangle-fill.py
+++ 241125/사각형 채우기/rectangle-fill.py
@@ -1,9 +1,10 @@
+MOD = 10007
 def recurse(n):
     if n <= 2:
         return n
     left = recurse(n-1) if dp[n-1] == -1 else dp[n-1]
     right = recurse(n-2) if dp[n-2] == -1 else dp[n-2]
-    dp[n] = left + right
+    dp[n] = (left + right)%MOD
     return dp[n]
 
 n = int(input())
