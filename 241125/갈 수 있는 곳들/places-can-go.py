846b8856dab31a478d6a41496688d72fda5cfd56
SeongWooJeong <120567895+qda-sw@users.noreply.github.com>
2024-11-25 20:38:53 +0900

    Title: 갈 수 있는 곳들, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241125/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def can_move(y, x):
+    return 0 <= y < n and 0 <= x < n and grid[y][x] == 0
+dys = [0, 1, 0, -1]
+dxs = [1, 0, -1, 0]
+def bfs(y, x):
+    queue = deque()
+    queue.append((y, x))
+    result = 0
+    while queue:
+        y, x = queue.popleft()
+        for dy, dx in zip(dys, dxs):
+            ny, nx = y + dy, x + dx
+            if not can_move(ny, nx):
+                continue
+            grid[ny][nx] = 1
+            queue.append((ny, nx))
+            result += 1
+    return result
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+start_points = [(y-1, x-1) for y, x in [map(int, input().split()) for _ in range(k)]]
+
+result = 0
+for y, x in start_points:
+    result += bfs(y, x)
+print(result)
